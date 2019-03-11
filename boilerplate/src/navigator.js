0c55d483f18e4ead3cf93b963ce311c6a63a06ab
nomad-lab <nomadlab.id@gmail.com>
2019-03-11 09:22:15 +0700

    root navigator

--- boilerplate/src/navigator.js
+++ boilerplate/src/navigator.js
@@ -1,15 +1,66 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation'
+import {
+  createStackNavigator,
+  createAppContainer,
+  createBottomTabNavigator,
+} from 'react-navigation'
+import { Image } from 'react-native'
 
 // import screens
+import postScreen from './screens/Example'
+import profileScreen from './screens/Profile'
 
-import exampleScreen from './screens/Example'
+// import icons
+const searchIcon = require('./assets/Icons/Search.png')
 
 const defaultNavigationOptions = { header: null }
+const Tabs = createBottomTabNavigator(
+  {
+    Post: postScreen,
+    Profile: profileScreen,
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: tintColor => {
+        const { routeName } = navigation.state
+        let iconName
+
+        switch (routeName) {
+          case 'Profile':
+            iconName = searchIcon
+            break
+          case 'Post':
+            iconName = searchIcon
+            break
+          default:
+            break
+        }
+
+        return tabBarIcon({ iconName, tintColor })
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'red',
+      inactiveTintColor: '#bbb',
+      labelStyle: {
+        fontSize: 10,
+      },
+      style: {
+        backgroundColor: '#ffffff',
+        paddingVertical: 5,
+        borderWidth: 0,
+      },
+      allowFontScaling: true,
+      showIcon: true,
+    },
+    initialRouteName: 'Post',
+  },
+)
 
 const AppNavigator = createStackNavigator(
-  { Example: { screen: exampleScreen } },
+  { Home: { screen: Tabs } },
+  { Post: { screen: postScreen } },
   {
-    initialRouteName: 'Example',
+    initialRouteName: 'Post',
     navigationOptions: defaultNavigationOptions,
   },
 )
