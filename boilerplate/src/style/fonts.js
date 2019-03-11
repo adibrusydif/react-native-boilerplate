70618fab9462e8147d98dc447c91f69b319c36de
nomad-lab <nomadlab.id@gmail.com>
2019-03-11 09:25:36 +0700

    global fonts

--- boilerplate/src/style/fonts.js
+++ boilerplate/src/style/fonts.js
@@ -1,17 +1,18 @@
 import color from './color'
+import style from './defaultStyle'
 
 const fonts = {
   header: {
-    fontSize: 25,
+    fontSize: style.FONT_SIZE_TITLE,
     fontWeight: 'bold',
   },
-  subHeader: { fontSize: 23 },
+  subHeader: { fontSize: style.FONT_SIZE },
   body: {
-    fontSize: 12,
+    fontSize: style.FONT_SIZE_SMALL,
     fontWeight: 'normal',
   },
-  whiteText: { color: '#fff' },
-  darkText: { color: color.mainFontColor },
+  whiteText: { color: color.white },
+  darkText: { color: color.primaryFontColor },
   greyText: { color: color.grey },
 }
 
